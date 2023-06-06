5ca4414b6b308bd3616d8c5228fc8ba573a34a24
aleongit <aleix.leon@gmail.com>
2023-06-06 17:04:36 +0200

    Step 1. Add a Trusted User

--- sql/test.sql
+++ sql/test.sql
@@ -1,4 +1,5 @@
 
+-- ## Tutorial 0 - Get it Running ______________________________________________
 create schema api;
 
 create table api.todos (
@@ -18,6 +19,18 @@ grant select on api.todos to web_anon;
 create role authenticator noinherit login password 'mysecretpassword';
 grant web_anon to authenticator;
 
--------------------------------------------------------------------
 
+-- ## Tutorial 1 - The Golden Key ______________________________________________
+
+create role todo_user nologin;
+grant todo_user to authenticator;
+
+grant usage on schema api to todo_user;
+grant all on api.todos to todo_user;
+grant usage, select on sequence api.todos_id_seq to todo_user;
+
+
+
+
+-- query test ______________________________________________
 SELECT * FROM api.todos;
--- tutorial1.md
+++ tutorial1.md
@@ -1,7 +1,18 @@
 ## Tutorial 1 - The Golden Key
 
-
 ### Step 1. Add a Trusted User
+
+- The previous tutorial created a **web_anon** role in the database with which to execute anonymous web requests. 
+- Let’s make a role called **todo_user** for users who authenticate with the API. This role will have the authority to do anything to the todo list.
+
+```
+create role todo_user nologin;
+grant todo_user to authenticator;
+
+grant usage on schema api to todo_user;
+grant all on api.todos to todo_user;
+grant usage, select on sequence api.todos_id_seq to todo_user;
+```
 
 ### Step 2. Make a Secret
 
